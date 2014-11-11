8d399b8c40e995e2814f8b811cf9a56b7b1d7abf
Kern0 <localhost@kern0.ru>
2014-11-12 02:36:36 +0300

    Add config.js

---
+++ config.js
@@ -0,0 +1,22 @@
+module.exports = {
+	
+	srv: {
+		main: {
+			host: 'srv1.greencubes.org',
+			port: 25565
+		},
+		api: {
+			host: 'api.greencubes.org',
+			port: 443
+		},
+		help: {
+			host: 'help.greencubes.org',
+			port: 443
+		},
+		web: {
+			host: 'greencubes.org',
+			port: 443
+		}
+	}
+	
+};
